5ebfa3ad6d4304969fe6de5fc8aa1f30e2403f9e
qiuxue.wu <qiuxue.wu@lcfuturecenter.com>
2021-05-10 17:52:50 +0800

    init

---
+++ 1.flattern.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: Qiuxue.Wu - LCFC
+ * @Date: 2021-05-10 09:36:14
+ * @LastEditors: Qiuxue.Wu - LCFC
+ * @LastEditTime: 2021-05-10 10:15:39
+ * @Description: file content
+ * @FilePath: \algorithms\1.flattern.js
+ */
+
+// 将数组展平
+const arr = [1, 2, [[3, 4], 5]]
+
+
+// 递归法
+function flattern1(arr) {
+    return [].concat(...arr.map(item => {
+        return Array.isArray(item) ? flattern1(item) : item
+    }))
+}
+
+console.log(flattern1(arr)) // [1,2,3,4,5]
+
+
+// 非递归法
+function flattern2(arr = []) {
+    let stack = arr.slice()
+    let result = []
+    while (stack.length > 0) {
+        const last = stack.pop()
+        if (Array.isArray(last)) {
+            stack.push(...last)
+        } else {
+            result.push(last)
+        }
+    }
+    return result.reverse()
+}
+
+console.log(flattern2(arr)) // [1,2,3,4,5]
+
+
+
