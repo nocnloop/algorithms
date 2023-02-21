07a472d75dbe5618b5a8aaa807eff3c3ecc73821
qiuxue.wu <qiuxue.wu@lcfuturecenter.com>
2023-02-21 11:36:22 +0800

    简化代码格式

--- 1.flattern.js
+++ 1.flattern.js
@@ -2,41 +2,30 @@
  * @Author: Qiuxue.Wu - LCFC
  * @Date: 2021-05-10 09:36:14
  * @LastEditors: Qiuxue.Wu - LCFC
- * @LastEditTime: 2021-05-10 10:15:39
+ * @LastEditTime: 2023-02-21 11:35:36
  * @Description: file content
- * @FilePath: \algorithms\1.flattern.js
+ * @FilePath: /algorithms/1.flattern.js
  */
 
 // 将数组展平
-const arr = [1, 2, [[3, 4], 5]]
-
+const arr = [1, 2, [[3, 4], 5]];
 
 // 递归法
 function flattern1(arr) {
-    return [].concat(...arr.map(item => {
-        return Array.isArray(item) ? flattern1(item) : item
-    }))
+  return [].concat(...arr.map((item) => (Array.isArray(item) ? flattern1(item) : item)));
 }
 
-console.log(flattern1(arr)) // [1,2,3,4,5]
-
+console.log(flattern1(arr)); // [1,2,3,4,5]
 
 // 非递归法
 function flattern2(arr = []) {
-    let stack = arr.slice()
-    let result = []
-    while (stack.length > 0) {
-        const last = stack.pop()
-        if (Array.isArray(last)) {
-            stack.push(...last)
-        } else {
-            result.push(last)
-        }
-    }
-    return result.reverse()
+  let stack = arr.slice();
+  let result = [];
+  while (stack.length > 0) {
+    const last = stack.pop();
+    Array.isArray(last) ? stack.push(...last) : result.push(last);
+  }
+  return result.reverse();
 }
 
-console.log(flattern2(arr)) // [1,2,3,4,5]
-
-
-
+console.log(flattern2(arr)); // [1,2,3,4,5]
